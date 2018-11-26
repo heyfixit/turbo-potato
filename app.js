1672d6aa7a2c7e52533e02e1b0ccde61d9802756
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2018-11-26 07:19:20 -0500

    Initial Commit
    Implemented basic expressjs server with route that pipes youtube-dl to response.

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const { spawn } = require('child_process');
+
+app.get('/', (req, res) => {
+  res.send('\
+    <html>\
+      Hello World!<br />\
+      <a href="/test_pipe">Test piping youtube-dl output.</a>\
+    </html>\
+  ');
+});
+
+app.get('/test_pipe', (req, res) => {
+  const ydl = spawn('youtube-dl', [ '-f', 'best', 'https://www.youtube.com/watch?v=fz6yhwjiWLc', '-o', '-' ]);
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4'
+  });
+  ydl.stdout.pipe(res);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
