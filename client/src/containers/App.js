6a79554c4c432faec124012ebf1202cdd16bc771
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-01-27 06:21:44 -0500

    removed material-ui and map
    decided to keep it to flexbox and my own styles for now

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { simpleAction } from 'actions/simpleAction';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
 
-  simpleAction = (event) => {
+  simpleAction = () => {
     this.props.simpleAction();
   }
 
   render() {
     return (
       <div className="App">
-        <CssBaseline />
-        <Button variant="contained" color="primary">
+        <button variant="contained" color="primary">
           Hello World
-        </Button>
+        </button>
       </div>
     );
   }
